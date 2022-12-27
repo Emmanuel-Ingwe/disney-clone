e9c0056f1b8a2762fb437ddabcb10cb3a00e2068
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2022-12-27 17:04:22 +0100

    app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/"></Route>
+          <Login />
+        </Switch>
+      </Router>
 
     </div>
   );

13cba345729fa6651cebde3a798dda0ac5e778e3
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2022-12-28 09:46:23 +0100

    link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import About from "./components/About";
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="about" element={<About />} />
       </Routes>
 
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,15 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Login = (props) => {
     return <Container>
         <Content>Content</Content>
+        <div>
+            <h1>This is the home page</h1>
+            <Link to="about">Click to view our about page</Link>
+        </div>
     </Container>;
+
 };
 
 const Container = styled.section`
@@ -12,7 +18,7 @@ display: flex;
 ;`;
 
 const Content = styled.div`
-    
+
 `;
 
 export default Login;

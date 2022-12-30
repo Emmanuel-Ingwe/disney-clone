c4c72fd4c9b2ed77a74dbc72b432370fa5a4ab66
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2022-12-30 08:04:27 +0100

    styles__bg-img

--- src/components/Login.js
+++ src/components/Login.js
@@ -15,10 +15,20 @@ const Login = (props) => {
 const Container = styled.section`
 overflow: hidden;
 display: flex;
-;`;
+flex-direction: column;
+text-align: center;
+height: 100vh;
+`;
 
 const Content = styled.div`
-
+    margin-bottom: 10vw;
+    width: 100%;
+    position: relative;
+    min-height: 100vh;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
 
 export default Login;

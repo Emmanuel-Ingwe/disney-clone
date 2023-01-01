141ef8f30903083a93a682ba1071657ef8a3867a
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-01 16:58:43 +0100

    Menu/home a

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import styled from "styled-components";
 
 const Header = (props) => {
-    return <Nav>Header</Nav>;
+    return <Nav>
+        <Logo>
+            <img src="/images/logo.svg" alt="Disney" />
+        </Logo>
+        <NavMenu>
+            <a href="/home">
+                <img src="/images/home-icon.svg" alt="HOME" />
+            </a>
+            <span>HOME</span>
+        </NavMenu>
+    </Nav>;
 };
 
 const Nav = styled.nav`
@@ -11,6 +21,43 @@ const Nav = styled.nav`
     right: 0;
     height: 70px;
     background-color: #090b13;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 36px;
+    letter-spacing: 16px;
+    z-index: 3;
+`;
+
+const Logo = styled.a`
+    padding: 0;
+    width: 80px;
+    margin-top: 4px;
+    max-height: 70px;
+    font-size: 0;
+    display: inline-block;
+     
+    img {
+        display: block;
+        width: 100%;
+    }
+`;
+
+const NavMenu = styled.div`
+    align-items: center;
+    display: flex;
+    flex-flow: row nowrap;
+    height: 100%;
+    justify-content: flex-end;
+    margin: 0px;
+    padding: 0px;
+    position: relative;
+    margin-right: auto;
+    margin-left: 25px;
+
+    @media (max-width: 768px) {
+        display: none;
+    }
 `;
 
 export default Header;

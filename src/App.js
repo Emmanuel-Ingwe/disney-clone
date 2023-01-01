8804c7ebb7178c9e476f34703a0fdfc25dd35c91
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-01 15:48:49 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import About from "./components/About";
+import Header from "./components/Header";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route path="" element={<Header />} />
         <Route path="/" element={<Login />} />
-        <Route path="about" element={<About />} />
       </Routes>
 
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Header = (props) => {
+    return <Nav>Header</Nav>;
+};
+
+const Nav = styled.nav`
+    
+`;
+
+export default Header;

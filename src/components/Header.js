a134aa73a994b7f520f0e42c2c776a342a6d5e7f
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-02 22:41:09 +0100

    span

--- src/components/Header.js
+++ src/components/Header.js
@@ -55,6 +55,44 @@ const NavMenu = styled.div`
     margin-right: auto;
     margin-left: 25px;
 
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+            min-width: 20px;
+            width: 20px;
+        }
+
+        span {
+            color: rgb(249, 249, 249);
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0px;
+            white-space: nowrap;
+            position: relative;
+        }
+
+        &:before {
+            background-color: rgb(249, 249, 249);
+            border-radius: 0px 0px 4px 4px;
+            bottom: -6px;
+            content: "";
+            height: 2px;
+            opacity: 0;
+            position: absolute;
+            right: 0px;
+            transform-origin: ;
+            transform: scaleX(0);
+            transition: all 250ms cubic-bezier(0.25, 0.46, 0.45) 0s;
+            visibility: hidden;
+            width: auto;
+        }
+    }
+
     /* @media (max-width: 768px) {
         display: none;
     } */

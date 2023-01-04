77fc57fe8f6033fb374ceeee1e008838be72f59d
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-04 07:35:53 +0100

    header Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,41 +32,52 @@ const Header = (props) => {
         );
     };
 
-    return <Nav>
-        <Logo>
-            <img src="/images/logo.svg" alt="Disney" />
-        </Logo>
-        {!userName ?
-            <Login onClick={handleApp}>Login</Login>
-            : <></>}
-        <NavMenu>
-            <a href="/home">
-                <img src="/images/home-icon.svg" alt="HOME" />
-                <span>HOME</span>
-            </a>
-            <a href="/">
-                <img src="/images/search-icon.svg" alt="SEARCH" />
-                <span>SEARCH</span>
-            </a>
-            <a href="/">
-                <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
-                <span>WATCHLIST</span>
-            </a>
-            <a href="/">
-                <img src="/images/original-icon.svg" alt="ORIGINALS" />
-                <span>ORIGINALS</span>
-            </a>
-            <a href="/">
-                <img src="/images/movie-icon.svg" alt="MOVIES" />
-                <span>MOVIES</span>
-            </a>
-            <a href="/">
-                <img src="/images/series-icon.svg" alt="SERIES" />
-                <span>SERIES</span>
-            </a>
-        </NavMenu>
-        <Login onClick={handleApp}>Login</Login>
-    </Nav>;
+    return (
+        <Nav>
+            <Logo>
+                <img src="/images/logo.svg" alt="Disney+" />
+            </Logo>
+
+            {!userName ? (
+                <Login onClick={handleApp}>Login</Login>
+            ) : (
+                <>
+                    <NavMenu>
+                        <a href="/home">
+                            <img src="/images/home-icon.svg" alt="HOME" />
+                            <span>HOME</span>
+                        </a>
+                        <a href="/">
+                            <img src="/images/search-icon.svg" alt="SEARCH" />
+                            <span>SEARCH</span>
+                        </a>
+                        <a href="/">
+                            <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+                            <span>WATCHLIST</span>
+                        </a>
+                        <a href="/">
+                            <img src="/images/original-icon.svg" alt="ORIGINALS" />
+                            <span>ORIGINALS</span>
+                        </a>
+                        <a href="/">
+                            <img src="/images/movie-icon.svg" alt="MOVIES" />
+                            <span>MOVIES</span>
+                        </a>
+                        <a href="/">
+                            <img src="/images/series-icon.svg" alt="SERIES" />
+                            <span>SERIES</span>
+                        </a>
+                    </NavMenu>
+                    {/* <SignOut> */}
+                    <UserImg src={userPhoto} alt={userName} />
+                    {/* <DropDown>
+                            <span onClick={handleApp}>Sign out</span>
+                        </DropDown> */}
+                    {/* </SignOut> */}
+                </>
+            )}
+        </Nav>
+    );
 };
 
 const Nav = styled.nav`
@@ -174,7 +185,12 @@ const Login = styled.a`
         color: #000;
         border-color: transparent;
   }
-        `;
+`;
+
+const UserImg = styled.img`
+  height: 100%;
+`;
+
 
 
 export default Header;

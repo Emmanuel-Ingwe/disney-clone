207514a60ef2856786cbc3430fd5473b46ce46c8
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-04 07:52:27 +0100

    __

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
@@ -10,6 +11,15 @@ const Header = (props) => {
     const navigate = useNavigate();
     const userName = useSelector(selectUserName);
     const userPhoto = useSelector(selectUserPhoto);
+
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if (user) {
+                setUser(user);
+                navigate.push("/home");
+            }
+        });
+    }, [userName]);
 
     const handleApp = () => {
         auth
@@ -68,12 +78,12 @@ const Header = (props) => {
                             <span>SERIES</span>
                         </a>
                     </NavMenu>
-                    {/* <SignOut> */}
-                    <UserImg src={userPhoto} alt={userName} />
-                    {/* <DropDown>
+                    <SignOut>
+                        <UserImg src={userPhoto} alt={userName} />
+                        <DropDown>
                             <span onClick={handleApp}>Sign out</span>
                         </DropDown> */}
-                    {/* </SignOut> */}
+                    </SignOut>
                 </>
             )}
         </Nav>
@@ -169,26 +179,62 @@ const NavMenu = styled.div`
   /* @media (max-width: 768px) {
             display: none;
   } */
-        `;
+                            `;
 
 const Login = styled.a`
         background-color: rgba(0, 0, 0, 0.6);
-        padding: 8px 16px;
-        text-transform: uppercase;
-        letter-spacing: 1.5px;
-        border: 1px solid #f9f9f9;
-        border-radius: 4px;
-        transition: all 0.2s ease 0s;
-        cursor:pointer;
-        &:hover {
-            background - color: #f9f9f9;
-        color: #000;
-        border-color: transparent;
+                        padding: 8px 16px;
+                        text-transform: uppercase;
+                        letter-spacing: 1.5px;
+                        border: 1px solid #f9f9f9;
+                        border-radius: 4px;
+                        transition: all 0.2s ease 0s;
+                        cursor:pointer;
+                        &:hover {
+                            background - color: #f9f9f9;
+                        color: #000;
+                        border-color: transparent;
   }
-`;
+                        `;
 
 const UserImg = styled.img`
-  height: 100%;
+                        height: 100%;
+                        `;
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 10px;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+`;
+
+const SignOut = styled.div`
+  position: relative;
+  height: 48px;
+  width: 48px;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  ${UserImg} {
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+  }
+  &:hover {
+    ${DropDown} {
+      opacity: 1;
+      transition-duration: 1s;
+    }
+  }
 `;
 
 

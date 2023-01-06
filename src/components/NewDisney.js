316e54ccfd555d83ab177a326e62500ec078204c
Emmanuel-Ingwe <ingwemmanuel@gmail.com>
2023-01-06 12:11:03 +0100

    movies

--- src/components/NewDisney.js
+++ src/components/NewDisney.js
@@ -33,6 +33,7 @@ const Content = styled.div`
   grid-gap: 25px;
   gap: 25px;
   grid-template-columns: repeat(4, minmax(0, 1fr));
+  
   @media (max-width: 768px) {
     grid-template-columns: repeat(2, minmax(0, 1fr));
   }
